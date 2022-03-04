cd96523fa52f55db882d4777f5eca93d94219022
Thone Liu <thorne_lew@126.com>
2022-03-04 18:14:59 +0800

    delete dist

--- dist/utils/hexer.d.ts
+++
@@ -1,1 +0,0 @@
-export default function hexer(input: string): string;
--- dist/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function fixedToInt(f: string | number, decimal: number): string;
-export declare function intToFixed(i: string | number, decimal: number): string;
-export declare function fixdToDe(new_value: string | number, decimal: number): string;
