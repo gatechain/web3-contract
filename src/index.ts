a5cf9bc1b354aed7315767951fd1e73a9e7ed7ff
Thone Liu <thorne_lew@126.com>
2022-12-29 15:35:32 +0800

    delete gatewallet and ERC20, Perpetual

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,54 @@
-import { Signer, providers } from "ethers"
-import { ERC20 } from "./contract/ERC20";
-import Perpetual from "./contract/Perpetual";
-export * from './utils'
-import gatewallet from 'gatewallet'
+import { Signer, providers } from "ethers";
+export * from "./utils";
+
 export interface Config {
   [key: number]: {
-    perpetualContract: {
-      address: string
-    },
-    [key: string]: any,
-  }
-  GateWalletConfig?: {
-    METAMASK_MESSAGE?: string
-    EIP_712_PROVIDER?: string
-    EIP_712_VERSION?: string
-    CONTRACT_ADDRESSES?: {
-      GateChain: string,
-      WithdrawalDelayer: string
-    }
-    CREATE_ACCOUNT_AUTH_MESSAG?: string
-  }
+    [key: string]: any;
+  };
 }
 
 export class HipoContract {
-  public provider: providers.Provider
-  public currAccount: string
-  public signer: Signer
-  public perpetual: Perpetual
-  public ERC20: ERC20
-  public config: Config
-  public chainId: number
-  private gateWallet?: any
-  private gateAddress?: string
+  public provider: providers.Provider;
+  public currAccount: string;
+  public signer: Signer;
+  public config: Config;
+  public chainId: number;
 
-  [key: string]: any
+  [key: string]: any;
 
   constructor(props: any) {
-    const resultPorps = {
-      contract: this
-    }
-    this.currAccount = props.currAccount
-    this.provider = props.provider
-    this.config = props.config
-    this.chainId = props.chainId
-    this.signer = props.provider.getSigner(props.currAccount)
-
-    this.ERC20 = new ERC20(resultPorps)
-    this.perpetual = new Perpetual(resultPorps)
+    this.currAccount = props.currAccount;
+    this.provider = props.provider;
+    this.config = props.config;
+    this.chainId = props.chainId;
+    this.signer = props.provider.getSigner(props.currAccount);
   }
 
   public registerContract(ContractClass: any): void {
-    const attr = ContractClass.contractName
+    const attr = ContractClass.contractName;
     if (this[attr]) {
-      console.warn(attr + ' already exists.')
-      return
+      console.warn(attr + " already exists.");
+      return;
     }
-    this[attr] = new ContractClass({ contract: this })
-    this[attr].name = attr
+    this[attr] = new ContractClass({ contract: this });
+    this[attr].name = attr;
   }
 
   public sign(value: string) {
-    return this.signer.signMessage(value)
+    return this.signer.signMessage(value);
   }
 
   public setAccount(account: string) {
-    this.currAccount = account
-    this.signer = (this.provider as any).getSigner(account)
+    this.currAccount = account;
+    this.signer = (this.provider as any).getSigner(account);
   }
 
   public setChainId(chainId: number) {
-    this.chainId = chainId
+    this.chainId = chainId;
   }
 
   public setProvider(provider: providers.Provider) {
-    this.provider = provider
-    this.signer = (this.provider as any).getSigner(this.currAccount)
-  }
-
-  public async createWalletFromGateChainAccount(privateKeyHex?: string | null): Promise<{ gateWallet: any, gateAddress: string }> {
-    const { gateWallet, gateAddress } = await gatewallet.createWalletFromGateChainAccount(this.signer, this?.config[this.chainId]?.GateWalletConfig, privateKeyHex || null)
-    this.gateWallet = gateWallet
-    this.gateAddress = gateAddress
-    return { gateWallet, gateAddress }
-  }
-
-  public getGateWallet() {
-    if (this.gateWallet) {
-      return this.gateWallet
-    }
-  }
-
-  public getGateAddress() {
-    if (this.gateAddress) {
-      return this.gateAddress
-    }
-  }
-
-  public signCreateAccountAuthorization() {
-    if (!this.gateWallet) {
-      throw new Error('Please use hipocontract in advance Createwalletfrometheraccount initialize gatewallet.')
-    }
-
-    return this.gateWallet.signCreateAccountAuthorization(this.provider, this.signer)
+    this.provider = provider;
+    this.signer = (this.provider as any).getSigner(this.currAccount);
   }
 }
